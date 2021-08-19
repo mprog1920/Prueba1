de7230716ce645a08b7ffd0a52baeed287af5ce1
Montse Viyuela <mprog1920@gmail.com>
2021-08-19 19:37:15 +0200

    Definida variable x

--- Prueba1.java
+++ Prueba1.java
@@ -15,6 +15,7 @@ public class Prueba1
     public static void main(String[] args){
         System.out.println("Hello desde BlueJ");
         System.out.println("Hola desde BlueJ");
+        int x = 10;
          
     }
 }
