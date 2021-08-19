82d5cf472e188f9d1740176548b52ef07ba2589d
Montse Viyuela <mprog1920@gmail.com>
2021-08-19 19:03:34 +0200

    Commit inicial

---
+++ Prueba1.java
@@ -0,0 +1,19 @@
+
+/**
+ *  
+ * 
+ * @author  
+ * @version  
+ */
+public class Prueba1
+{
+     
+
+    /**
+     *  
+     */
+    public static void main(String[] args){
+        System.out.println("Hello desde BlueJ");
+         
+    }
+}
