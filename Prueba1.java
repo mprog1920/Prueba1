da8f50a7ac49d56c4bfa2c94d41b756f0b8fe915
Montse Viyuela <mprog1920@gmail.com>
2021-08-20 17:45:28 +0200

    Eliminar saludo Hello

--- Prueba1.java
+++ Prueba1.java
@@ -10,7 +10,6 @@ public class Prueba1
      *  
      */
     public static void main(String[] args){
-        System.out.println("Hello desde BlueJ");
         System.out.println("Hola desde BlueJ");
         int x = 20;
         x++;
